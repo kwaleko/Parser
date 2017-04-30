20cef4b0acb9ee2336aa36a7becb7788216ecf66
Kwaleko <khaledalomar@tutanota.com>
2017-04-30 18:15:59 +0300

    add comments

--- Parser.hsproj/Parser.hs
+++ Parser.hsproj/Parser.hs
@@ -37,7 +37,8 @@ instance Functor Parser where
 
 {- pure transorm a value that always succeed with this value as its result, without consuming any of the input string 
 
-  <*> applies a parser that returns a function to a parser that returns an argument to give a parser that returns the result of applying the function to the argument, and only succed if all the compponents succeed -}   
+  <*> applies a parser that returns a function to a parser that returns an argument to give a parser that 
+  returns the result of applying the function to the argument, and only succed if all the compponents succeed -}   
 instance Applicative Parser where
   -- pure :: a -> Parser a
   pure val = P (\inp -> [(val,inp)])
@@ -78,11 +79,17 @@ digit = sat isDigit
 isNotDigit x = not $ isDigit x
 
 {- AllDigit is a parser of string that parse all digit in a given string using the below method:
--try a parser  (end >>= \_ -> return "") that check if the list of character has been consumed, if this parser fail then the next parser will be executed, if succeeded then it will return a parser of empty string with the remaining string is empty as well.
+-try a parser  (end >>= \_ -> return "") that check if the list of character has been consumed, if this parser
+ fail then the next parser will be executed, if succeeded then it will return a parser of empty string with the
+  remaining string is empty as well.
 
--the second parser is ((sat isNotDigit) >>= \_ -> allDigits) that try to parse a character and succeed as long as the character is not digit, or fail otherwise. when succeeded the parsed character will ignore by monadic bind then the function allDigit will be called again, when it fails then it will try the next parser.
+-the second parser is ((sat isNotDigit) >>= \_ -> allDigits) that try to parse a character and succeed as long 
+as the character is not digit, or fail otherwise. when succeeded the parsed character will ignore by monadic bind 
+then the function allDigit will be called again, when it fails then it will try the next parser.
 
--the thrid parser ((many digit) >>= \c ->  allDigits >>= \cs -> return (c++cs)) will try to parser list of digits, and this function will keep calling itself recusively till the end of the string then return the parsed characters and return it using the succeed parser. -}
+-the thrid parser ((many digit) >>= \c ->  allDigits >>= \cs -> return (c++cs)) will try to parser list of 
+digits, and this function will keep calling itself recusively till the end of the string then return the parsed 
+characters and return it using the succeed parser. -}
 allDigits :: Parser [Char]
 allDigits =( end >>= \_ -> return "" ) <|> ((sat isNotDigit) >>= \_ -> allDigits) <|> ( (many digit) >>= \c ->  allDigits >>= \cs -> return (c++cs))
 
