29eab87648b0edc5ac3cd8f6d4d1b29f7635e56f
Kwaleko <khaledalomar@tutanota.com>
2017-05-01 15:13:26 +0300

    update main

--- Parser.hsproj/Main.hs
+++ Parser.hsproj/Main.hs
@@ -2,11 +2,6 @@ import Parser
 import File 
 import System.IO
 
-main :: IO ()
-main = do
-  inpStr <- readFile "/Users/lambda/Documents/createFile.txt"
-  let [(result,_)] = parse allDigits inpStr
-  putStr result
   
 main1 :: IO ()
 main1 = do
@@ -15,10 +10,5 @@ main1 = do
   putStr result
   
 
-
-displaySubDirectory :: IO ()
-displaySubDirectory = do
-  dirs <- countEntries "/Users/lambda/Music/untitled folder 2"
-  print dirs
 
 
