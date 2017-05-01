5d8363ce3533356c2530ea5063c55ef3c8a7c5a0
Kwaleko <khaledalomar@tutanota.com>
2017-05-01 15:14:19 +0300

    update main

--- Parser.hsproj/Main.hs
+++ Parser.hsproj/Main.hs
@@ -2,7 +2,7 @@ import Parser
 import File 
 import System.IO
 
-  
+-- return the parsed digits in the given file  
 main1 :: IO ()
 main1 = do
   inpStr <- readFile "/Users/lambda/Documents/createFile.txt"
