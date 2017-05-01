8c329a50cdb4440c39eedb568c7b585142903e99
Kwaleko <khaledalomar@tutanota.com>
2017-05-01 15:19:35 +0300

    update main

--- Parser.hsproj/Main.hs
+++ Parser.hsproj/Main.hs
@@ -3,8 +3,8 @@ import File
 import System.IO
 
 -- return the parsed digits in the given file  
-main1 :: IO ()
-main1 = do
+main :: IO ()
+main = do
   inpStr <- readFile "/Users/lambda/Documents/createFile.txt"
   let [(result,_)] = parse parseDigits inpStr
   putStr result
