575096c02c558d206bca01ddab22cbd0e1eb68fd
Kwaleko <khaledalomar@tutanota.com>
2017-05-01 15:20:17 +0300

    update main

--- Parser.hsproj/Main.hs
+++ Parser.hsproj/Main.hs
@@ -2,7 +2,16 @@ import Parser
 import File 
 import System.IO
 
--- return the parsed digits in the given file  
+ 
+{- How to use the below function to parse digits in a given file:
+
+  -Create a txt file
+  -replace the path in the readFile to be the path for the created file
+  -go to cmd
+  -run ghci or stack ghci if you are using stack
+  -then load the Main file in the REPL ( :load Main.hs )
+  -now you can write main in the REPL and it will return the parsed string
+-}
 main :: IO ()
 main = do
   inpStr <- readFile "/Users/lambda/Documents/createFile.txt"
