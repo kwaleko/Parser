5323287b487207da7457969e816c7dd4864812c4
Kwaleko <khaledalomar@tutanota.com>
2017-05-31 19:41:53 +0300

    add functions

--- Parser.hsproj/DBInterface.hs
+++ Parser.hsproj/DBInterface.hs
@@ -1,14 +1,14 @@
 module                DBInterface   
                       (
-                       dbSchema
-                       ,getArticleTitle
-                      ) where
+                        addArticle
+                       ,createUser 
+                       ,dbSchema
+                       ,getArticle
+                      ) 
+                        where
   
 import                Control.Monad.Trans.Reader  (ask,ReaderT)
-import                Database.HDBC.Sqlite3       (
-                                                   connectSqlite3
-                                                  ,Connection
-                                                  )
+import                Database.HDBC.Sqlite3       (Connection)
 
 
 import qualified      DBUtils as Db
@@ -39,17 +39,38 @@ dbSchema  = do
           ,"CREATE TABLE IF NOT EXISTS users (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,username VARCHAR (20),name VARCHAR(20))"
         ]
         
--- Add user to the database
-createUser      :: User -> ReaderT Connection IO Int
+-- add new Article
+addArticle  ::    Article 
+               -> ReaderT Connection IO ()
+addArticle
+  article = do
+    conn <- ask
+    Db.sqlRun sql [ Db.toSql (artId    article)
+                   ,Db.toSql (artTitle article)
+                   ,Db.toSql (artBody  article) 
+                   ]
+    where
+      sql = "insert into Articles (Title, Content ,UserId) values (?,?,?)"                
+                   
+ -- Add user to the database
+createUser      ::   User 
+                  -> ReaderT Connection IO ()
 createUser  
   user =  do
     conn <- ask
-    let sqlvalues = [
-                HDBC
-    ]
-    Db.sqlRun sql 
-        
-{- main :: IO ()
-main = do
-  conn <- connectSqlite3 "db.txt"
-  Db.sqlCommit   conn  (dbSchema conn ) -}
+    Db.sqlRun sql [Db.toSql (userAccount user)
+                   ,Db.toSql (userName   user)]
+                   
+    where
+      sql = " INSERT INTO users (account, name) VALUES (?,?)"
+ 
+
+{- 
+getArticle   ::    Id -> ReaderT Connection IO (Maybe Article)               
+getArticle id = do
+    conn <- ask
+    Db.sqlQueryOne sql [Db.toSql id] transform
+    where 
+      sql = "SELECT (id,title,content) FROM articles where id = ? "
+      transform = \xs -> Article (Db.fromSqlToInt (xs !! 0)) (Db.fromSqlToString (xs !! 1)) (Db.fromSqlToString (xs !! 2)) -}
+          
