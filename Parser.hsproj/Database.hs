8245e8e39d4520562b7edaf0dc906482b2078467
Kwaleko <khaledalomar@tutanota.com>
2017-06-02 22:54:05 +0300

    Delete some files

--- Parser.hsproj/Database.hs
+++ Parser.hsproj/Database.hs
@@ -8,30 +8,35 @@ import qualified         Types    as T
 import qualified         HandleIO as H
 import qualified         Parser   as P
 
+
+
 -- Get Articles from File database.
-getArticlesFromDB :: FilePath -> T.HandleIO (Maybe [T.Article])
+getArticlesFromDB :: FilePath -> T.HandleIO (Either String [T.Article])
 getArticlesFromDB path = do
   handle <- H.openF path ReadMode
   str <- H.readF handle
-  let result = T.parse ((P.chunksToArticles . P.chunks)  <$>  (P.break ';')) str
-  return $ fst . head $ result
---  H.closeF handle
---  return val
+  let result = T.parse P.articles str
+  case result of
+    Right (out,_) -> return $ Right out
+    Left err -> return $ Left err
+
   
 -- get one article from database 
-getArticleById :: FilePath -> Int -> T.HandleIO (Maybe T.Article)
+getArticleById :: FilePath -> String -> T.HandleIO (Either String T.Article)
 getArticleById path id = do
   handle <- H.openF path ReadMode
   str <- H.readF handle
-  let result = T.parse ((P.articlesLookup . P.chunksToArticles . P.chunks)  <$>  (P.break ';')) str
-  let lookupList = fst . head $ result
-  return $  join $ lookup (Just id) <$> lookupList
+  let result = T.parse (P.article id) str
+  case result of
+    Right (out,_) -> return $ Right out
+    Left err -> return $ Left err
   
 
-getArticlesFromDB' :: ReaderT FilePath T.HandleIO (Maybe [T.Article])
+{- getArticlesFromDB' :: ReaderT FilePath T.HandleIO (Either String [T.Article])
 getArticlesFromDB' = ReaderT $ \file -> do
   handle <- H.openF file ReadMode
   str <- H.readF handle
-  let result = T.parse ((P.chunksToArticles . P.chunks)  <$>  (P.break ';')) str
+  let result = T.parse P.parseArticles str
   return $ fst . head $ result
+  -}
 
